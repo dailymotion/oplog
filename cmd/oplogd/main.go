212c6a65f07c792f814fb08ca52a976d769ec83b
Olivier Poitrey <rs@dailymotion.com>
2014-11-06 01:21:01 -0800

    Add --debug option

--- cmd/oplogd/main.go
+++ cmd/oplogd/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 var (
+	debug                = flag.Bool("debug", false, "Show debug log messages.")
 	listenAddr           = flag.String("listen", ":8042", "The address to listen on. Same address is used for both SSE(HTTP) and UDP APIs.")
 	mongoURL             = flag.String("mongo-url", "", "MongoDB URL to connect to.")
 	cappedCollectionSize = flag.Int("capped-collection-size", 104857600, "Size of the created MongoDB capped collection size in bytes (default 100MB).")
@@ -17,12 +18,17 @@ var (
 
 func main() {
 	flag.Parse()
-	log.SetLevel(log.DebugLevel)
+
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	}
 
 	ol, err := oplog.NewOpLog(*mongoURL, *cappedCollectionSize)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Infof("Listening on %s (UDP/TCP)", *listenAddr)
 
 	udpd := oplog.NewUDPDaemon(*listenAddr, ol)
 	go func() {
